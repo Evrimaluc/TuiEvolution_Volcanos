060e805aa89f206c2f887f7b75a7c1082867af79
Evrim Aluç <alucevrim@gmail.com>
2026-02-14 14:32:16 +0300

    Update simulationService.js

--- frontend/src/services/simulationService.js
+++ frontend/src/services/simulationService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000';
+const API_URL = 'https://tuievolution-volcanos-backend.onrender.com';
 
 export const runSimulation = async (volcanoData) => {
     try {
